96fb9873c2bdcb7d0f954c1e850707ae2cf8e4ce
NavControl <ivansm0303@gmail.com>
2025-10-26 19:04:02 -0600

    Subiendo todos los archivos del proyecto excluyendo venv

--- app_autolavado/modules/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Crea un Blueprint
-auth_bp = Blueprint('auth', __name__, 
-                    template_folder='templates',
-                    static_folder='static')
-
-# Importa las rutas desde el archivo routes.py para que se registren
-from . import routes
